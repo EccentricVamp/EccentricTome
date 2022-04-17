64f5c051dfc4fb33b6fb6b1a25cb0b9ba7e49dc6
EccentricVamp <EccentricVamp@gmail.com>
2022-04-17 17:58:11 -0500

    Update tome recipe to use registry object

--- forge/src/main/java/website/eccentric/tome/TomeRecipe.java
+++ forge/src/main/java/website/eccentric/tome/TomeRecipe.java
@@ -2,8 +2,6 @@ package website.eccentric.tome;
 
 import java.util.function.Consumer;
 
-import com.google.inject.Inject;
-
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
@@ -12,9 +10,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 
 public class TomeRecipe extends RecipeProvider {
-
-    @Inject
-    private TomeItem tomeItem;
     
     public TomeRecipe(DataGenerator generator) {
         super(generator);
@@ -23,7 +18,7 @@ public class TomeRecipe extends RecipeProvider {
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
         ShapelessRecipeBuilder
-            .shapeless(tomeItem)
+            .shapeless(EccentricTome.TOME.get())
             .requires(Items.BOOK)
             .requires(Blocks.BOOKSHELF)
             .save(consumer);
