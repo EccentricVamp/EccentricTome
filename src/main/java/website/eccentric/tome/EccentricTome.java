13715d215f5d9ca412f9fd5368ebd91550a2c3fb
EccentricVamp <EccentricVamp@gmail.com>
2022-09-04 09:58:34 -0500

    Remove unused import

--- src/main/java/website/eccentric/tome/EccentricTome.java
+++ src/main/java/website/eccentric/tome/EccentricTome.java
@@ -18,7 +18,6 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
